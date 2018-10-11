1b51e75ed7736293c59a4bc606e70c28388a823b
João Pedro Jericó de Andrade <jpjandrade@gmail.com>
2018-10-11 23:47:49 +0200

    Add stack / queue implementation

---
+++ cracking_coding_interview/03_stacks_queues.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, d):
+        self.data = d
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def pop(self):
+        if self.top is None:
+            raise AttributeError  # I didn't know which error to put here :-P
+        item = self.top.data
+        self.top = self.top.next
+        return item
+
+    def push(self, item):
+        t = Node(item)
+        t.next = self.top
+        self.top = t
+
+    def peek(self):
+        if self.top is None:
+            raise AttributeError
+
+        return self.top.data
+
+    def is_empty(self):
+        return self.top is None
+
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def add(self, data):
+        t = Node(data)
+        if self.last is not None:
+            self.last.next = t
+        self.last = t
+        if self.first is None:
+            self.first = self.last
+
+    def remove(self):
+        if self.first is None:
+            raise EOFError  # trying random errors :-P
+
+        item = self.first.data
+        self.first = self.first.next
+
+        if self.first is None:
+            self.last = None
+
+        return item
+
+    def peek(self):
+        if self.first is None:
+            raise EOFError
+        return self.first.data
+
+    def is_empty(self):
+        return self.first is None
