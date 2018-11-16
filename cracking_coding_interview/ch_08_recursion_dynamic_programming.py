b0bec5380e662aac71d05553f61ed7fdf19ebabf
João Pedro Jericó de Andrade <jpjandrade@gmail.com>
2018-11-16 13:44:53 +0100

    Add ex 8.2, robot on a grid. Fun one!

--- cracking_coding_interview/ch_08_recursion_dynamic_programming.py
+++ cracking_coding_interview/ch_08_recursion_dynamic_programming.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # ex 8.1
 
 
@@ -12,3 +14,47 @@ def stairs_comb(n):
             combinations = memo[i - 1] + memo[i - 2] + memo[i - 3]
             memo.append(combinations)
     return memo[n]
+
+# ex 8.2
+# this one was pretty fun to solve
+
+
+def generate_grid(r, c):
+    G = (np.random.binomial(n=1, p=0.8, size=(r, c)) - 1).tolist()
+    G[0][0] = 0
+    G[-1][-1] = 1
+    grid = {
+        'G': G,
+        'r': r,
+        'c': c
+    }
+    return grid
+
+
+def robot_on_a_grid(grid):
+    initial_x = 0
+    initial_y = 0
+    path = []
+    final_path, found = robot_search(initial_x, initial_y, path, grid)
+    if found:
+        return final_path
+    else:
+        return "No path found! :-("
+
+
+def robot_search(x, y, path, grid):
+    G = grid['G']
+    if x >= grid['r'] or y >= grid['c'] or G[x][y] == -1:
+        return path, False
+
+    if G[x][y] == 1:
+        return path, True
+
+    path_d, found_d = robot_search(x + 1, y, path + ['d'], grid)
+
+    if found_d:
+        return path_d, True
+
+    path_r, found_r = robot_search(x, y + 1, path + ['r'], grid)
+
+    return path_r, found_r
