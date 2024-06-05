094a2cd2da5ef79bc5853844c1f2424e27585ab8
João Pedro Jericó de Andrade <jpjandrade@gmail.com>
2024-06-05 22:15:19 +0200

    Add one more silly C++ leetcode

---
+++ cpp_crash_course/lc_1002_find_common_characters.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<string> commonChars(vector<string>& words) {
+    if (words.empty()) {
+      return {};
+    }
+    unordered_map<char, int> common_chars;
+    for (const char& c : words.at(0)) {
+      common_chars[c]++;
+    }
+
+    for (int i = 1; i < words.size(); ++i) {
+      unordered_map<char, int> curr_word;
+      for (const auto& c : words.at(i)) {
+        curr_word[c]++;
+      }
+      for (const auto& [c, count] : common_chars) {
+        if (curr_word[c] < common_chars[c]) {
+          common_chars[c] = curr_word[c];
+        }
+      }
+    }
+
+    vector<std::string> res;
+    for (const auto& [c, count] : common_chars) {
+      for (int i = 0; i < count; ++i) {
+        res.push_back(string(1, c));
+      }
+    }
+    return res;
+  }
+};
