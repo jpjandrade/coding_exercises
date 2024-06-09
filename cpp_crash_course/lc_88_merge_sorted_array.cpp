c985a71bba668a5595670c204f0db06978710df7
João Pedro Jericó de Andrade <jpjandrade@gmail.com>
2024-06-09 23:10:05 +0200

    Actually commit LC88

--- cpp_crash_course/lc_88_merge_sorted_array.cpp
+++ cpp_crash_course/lc_88_merge_sorted_array.cpp
@@ -1,8 +1,25 @@
 #include <vector>
 
 class Solution {
-public:
-    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
-        
+ public:
+  // Nums1 is size m + n and must be altered in place.
+  void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+    int i = m - 1, j = n - 1, k = n + m - 1;
+
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+      } else {
+        nums1[k] = nums2[j];
+        j--;
+      }
+      k--;
     }
+    while (j >= 0) {
+      nums1[k] = nums2[j];
+      j--;
+      k--;
+    }
+  }
 };
